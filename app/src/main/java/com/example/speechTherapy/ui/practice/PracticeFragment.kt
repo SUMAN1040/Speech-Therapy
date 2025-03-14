30ce1544ddb536d4823628efa1ef56533d5f5922
SUMAN1040 <UEMJ2003@GMAIL.COM>
2025-03-14 21:32:54 +0530

    Speech Therapy

---
+++ app/src/main/java/com/example/speechTherapy/ui/practice/PracticeFragment.kt
@@ -0,0 +1,42 @@
+package com.example.speechTherapy.ui.practice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.speechTherapy.databinding.FragmentPracticeBinding
+
+class PracticeFragment : Fragment() {
+
+    private var _binding: FragmentPracticeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val practiceViewModel =
+            ViewModelProvider(this).get(PracticeViewModel::class.java)
+
+        _binding = FragmentPracticeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        practiceViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
