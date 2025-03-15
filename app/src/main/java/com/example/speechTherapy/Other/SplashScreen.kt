c8d096d9fd57976a2fa0fb978a73fe5d59fa2988
SUMAN1040 <UEMJ2003@GMAIL.COM>
2025-03-16 02:14:18 +0530

    music

--- app/src/main/java/com/example/speechTherapy/Other/SplashScreen.kt
+++ app/src/main/java/com/example/speechTherapy/Other/SplashScreen.kt
@@ -6,35 +6,37 @@
 package com.example.speechTherapy.Other
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.speechTherapy.MainActivity
 import com.example.speechTherapy.R
 
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
 
-
+        //full screen
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+
+        //media player
+        val mediaPlayer = MediaPlayer.create(this, R.raw.welcome)
+        mediaPlayer.start()
+
+
+        //splash screen with time delays
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashScreen, MainActivity::class.java)
             startActivity(intent)
